970eb05f8cb1fb02446a9420cf5ac741ded82f66
Priswall <31701242+Priswall@users.noreply.github.com>
2022-03-28 22:33:05 -0400

    Add files via upload

---
+++ AntiAFK/AntiAFK.cs
@@ -0,0 +1,40 @@
+﻿using Exiled.API.Features;
+using System;
+using Server = Exiled.Events.Handlers.Server;
+using Player = Exiled.Events.Handlers.Player;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntiAFK
+{
+    public class AntiAFK : Plugin<Config>
+    {
+        private static readonly Lazy<AntiAFK> LazyInstance = new Lazy<AntiAFK>(() => new AntiAFK());
+        public static AntiAFK Instance = LazyInstance.Value;
+
+        private EventHandler eventHandler;
+
+        public override void OnEnabled()
+        {
+            eventHandler = new EventHandler();
+
+            Player.Left += eventHandler.OnLeft;
+            Player.Joined += eventHandler.OnJoined;
+            Server.RoundStarted += eventHandler.OnRoundStarted;
+            Server.RoundEnded += eventHandler.OnRoundEnded;
+        }
+
+        public override void OnDisabled()
+        {
+            Player.Left -= eventHandler.OnLeft;
+            Player.Joined -= eventHandler.OnJoined;
+            Server.RoundStarted -= eventHandler.OnRoundStarted;
+            Server.RoundEnded -= eventHandler.OnRoundEnded;
+
+            eventHandler.Destroy();
+            eventHandler = null;
+        }
+    }
+}
---
+++ AntiAFK/Config.cs
@@ -0,0 +1,18 @@
+﻿using Exiled.API.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntiAFK
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+
+        [Description("Sets the time (in seconds) that players are allowed to be AFK before getting booted")]
+        public int AFK_Time = 10;
+    }
+}
